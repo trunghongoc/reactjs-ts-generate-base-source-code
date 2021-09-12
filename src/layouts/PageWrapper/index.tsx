a6ec545ca3377e47077922ba9f61fc1922ab32ff
Trung Ho Ngoc - macpro 13 2018 <trung0194@gmai.com>
2021-09-12 22:27:09 +0700

    Remove class component

--- src/layouts/PageWrapper/index.tsx
+++ src/layouts/PageWrapper/index.tsx
@@ -1,26 +1,18 @@
-import { Component } from 'react'
+import { FC, useLayoutEffect } from 'react'
 
 import RouterService from 'services/router'
 
 import { IProps } from './type'
 
-export class PageWrapper extends Component<IProps> {
-  constructor(props: IProps) {
-    super(props)
+export const PageWrapper: FC<IProps> = ({
+  Page,
+  pageProps,
+  routeConfig,
+  location
+}: IProps): JSX.Element => {
+  useLayoutEffect((): void => {
+    RouterService.handleAfterRouteEnter(routeConfig, location)
+  }, [routeConfig, location])
 
-    this.handleBeforeRender()
-  }
-
-  handleBeforeRender = () => {
-    RouterService.handleAfterRouteEnter(
-      this.props.routeConfig,
-      this.props.location
-    )
-  }
-
-  render() {
-    const { Page, pageProps } = this.props
-
-    return <>{Page ? <Page {...pageProps} /> : <span />}</>
-  }
+  return Page ? <Page {...pageProps} /> : <span />
 }
